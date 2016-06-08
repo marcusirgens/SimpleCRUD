fe8d7ebff73d6bdb4fbbabd3adeb14a2f2a79388
Marcus Pettersen Irgens <marcus.pettersen.irgens@gmail.com>
2016-06-08 16:28:54 +0200

    Documented and added __toString requirement to Id interface

--- simple-crud/Id.php
+++ simple-crud/Id.php
@@ -2,9 +2,36 @@
     
 namespace marcuspi\SimpleCRUD;
 
+/**
+ * Interface for the CRUD system's unique Ids.
+ */
 interface Id
 {
+
+    /**
+     * Creates a new Id object
+     * 
+     * @access public
+     * @static
+     * @return self
+     */
     public static function make();
     
+    /**
+     * Restores an Id object from string
+     * 
+     * @access public
+     * @static
+     * @param string $id An id represented as a string
+     * @return self
+     */
     public static function restore($id);
+    
+    /**
+     * Requires this class to have a tostring function.
+     * 
+     * @access public
+     * @return string
+     */
+    public function __toString();
 }
